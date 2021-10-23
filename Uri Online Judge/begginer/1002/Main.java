a129aef520ef922a7f342bb089d90d3e6c047dd0
Gabriel Z. Salvi <gabriel.z.salvi@gmail.com>
2021-10-23 20:03:10 -0300

    add solved problem 1003 from URI

---
+++ Uri Online Judge/begginer/1002/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main 
+{ 
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        
+        double radius = scanner.nextDouble();
+        double area = 3.14159 * (radius * radius);
+
+        scanner.close();
+
+        System.out.printf("A=%.4f\n", area);
+    }
+}
