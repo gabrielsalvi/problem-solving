1181a3ac097526731bdaeb0a7a0cce5fd3e5d92a
Gabriel Z. Salvi <gabriel.z.salvi@gmail.com>
2021-11-24 14:18:41 -0300

    refactor: solving the problem using recursion

--- Beecrowd/beginner/1051/Main.java
+++ Beecrowd/beginner/1051/Main.java
@@ -4,29 +4,16 @@ public class Main {
 
     private static Double getTaxesValue(double salary) {
 
-        double taxes = 0.0;
+        if (salary > 4500.00)
+            return (salary - 4500.00) * (28 / 100.0) + getTaxesValue(salary - (salary - 4500.00));
 
-        while (salary > 2000.00) {
+        if (salary > 3000.00 && salary <= 4500.00)
+            return (salary - 3000) * (18 / 100.0) + getTaxesValue(salary - (salary - 3000));
 
-            if (salary > 4500.00) {
+        if (salary > 2000.00 && salary <= 3000.00)
+            return (salary - 2000) * (8 / 100.0) + getTaxesValue(salary - (salary - 2000));
 
-                taxes += (salary - 4500.00) * (28/100.0);
-                salary -= salary - 4500.00;
-
-            } else if (salary > 3000.00 && salary <= 4500.00) {
-
-                taxes += (salary - 3000) * (18/100.0);
-                salary -= salary - 3000;
-
-            } else if (salary > 2000.00 && salary <= 3000.00) {
-
-                taxes += (salary - 2000) * (8/100.0);
-                salary = salary - 2000;
-
-            }
-        }
-
-        return taxes;
+        return 0.0;
     }
 
     public static void main(String[] args) {
